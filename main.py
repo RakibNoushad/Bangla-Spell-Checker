aa15bf0b1e40cdbb2393e8e43875fe03d70fb681
RakibNoushad <rakibnoushad@gmail.com>
2020-08-18 22:41:24 +0600

    All file added

---
+++ main.py
@@ -0,0 +1,13 @@
+#import sys
+from spell import is_banan_correctly
+from spell import suggested_word
+
+
+values = input()
+for value in values.split(" "):
+    if not is_banan_correctly(value):
+        print("Spelling error: " + value)
+        print("Suggested word: " + str(suggested_word(value)))
+
+    elif is_banan_correctly(value):
+        print("Correct!")
